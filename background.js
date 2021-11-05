3a8636292bf73e8ac639118a20b5eeedd680af1e
Dmitrii <forofontovdima3@gmail.com>
2021-11-05 08:47:15 +0200

    add cross-browser

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.browserAction.onClicked.addListener(() => {
-    chrome.tabs.executeScript({ file: 'script.js', allFrames: true });
-});
+try {
+    browser.browserAction.onClicked.addListener(() => {
+        browser.tabs.executeScript({ file: 'script.js', allFrames: true });
+    });
+} catch (e) {};
+
+try {
+    chrome.browserAction.onClicked.addListener(() => {
+        chrome.tabs.executeScript({ file: 'script.js', allFrames: true });
+    });
+} catch (e) {};
