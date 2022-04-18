4d89cd084ac3a1c515bbf5603e4cf9e96b41b270
Dmitrii Forofontov <forofontov.dev@gmail.com>
2022-04-18 11:57:29 +0200

    fix: total error

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-if (!buttonsMenu) {   
-    var buttonsMenu = document.querySelector('.cZG6je');
+let volume = 1;
+let buttonsMenu;
+
+const findButtonsMenu = () => {
+    document.querySelectorAll('i').forEach((el) => {
+        if (el.textContent !== 'call_end') return;
+
+        buttonsMenu = el.parentElement.parentElement.parentElement.parentElement;
+    });
+};
+
+if (!buttonsMenu) {
+    findButtonsMenu();
 }
 
-if (buttonsMenu && (!document.querySelector('.add-button'))) {
+if (buttonsMenu && !document.querySelector('.add-button')) {
+    buttonsMenu.style.display = 'flex';
+    buttonsMenu.style.justifyContent = 'space-between';
+
     const noop = () => {};
 
-    // function which creates and add no meny button
+    // function which creates and add no menu button
     const createButton = ({ styleList = {}, content, classes = '', func = noop }) => {
         const button = document.createElement('button');
 
@@ -19,7 +33,8 @@ if (buttonsMenu && (!document.querySelector('.add-button'))) {
 
         func(button);
 
-        buttonsMenu.insertBefore(button, document.querySelector('[jsname="oE6Bi"]'));
+        const buttons = buttonsMenu.children;
+        buttons[1].insertAdjacentElement('afterend', button);
     };
 
     // creating MUTE / UNMUTE buttons
@@ -32,43 +47,49 @@ if (buttonsMenu && (!document.querySelector('.add-button'))) {
 
                 if (currentButton.classList.contains('muted')) {
                     button.innerText = 'MUTE';
-                    document
-                        .querySelectorAll('video, audio')
-                        .forEach((elem) => (elem.muted = false));
+                    document.querySelectorAll('video, audio').forEach((elem) => (elem.muted = false));
                 } else {
                     button.innerText = 'UNMUTE';
-                    document
-                        .querySelectorAll('video, audio')
-                        .forEach((elem) => (elem.muted = true));
+                    document.querySelectorAll('video, audio').forEach((elem) => (elem.muted = true));
                 }
                 currentButton.classList.toggle('muted');
             });
         },
     });
 
-    // creating Volume down button
+    // creating Volume DOWN button
     createButton({
         content: '-',
         classes: 'volume-button',
         func: (button) => {
             button.addEventListener('click', () => {
-                document
-                    .querySelectorAll('video, audio')
-                    .forEach((elem) => (elem.volume = elem.volume - 0.1));
+                document.querySelectorAll('video, audio').forEach((elem) => {
+                    const updVol = Math.round((elem.volume - 0.1) * 10) / 10;
+                    const vol = Math.max(0, updVol);
+                    elem.volume = vol;
+                    volume = vol;
+                });
             });
         },
     });
 
-    // creating Volume up button
+    // creating Volume UP button
     createButton({
         content: '+',
         classes: 'volume-button',
         func: (button) => {
             button.addEventListener('click', () => {
-                document
-                    .querySelectorAll('video, audio')
-                    .forEach((elem) => (elem.volume = elem.volume + 0.1));
+                document.querySelectorAll('video, audio').forEach((elem) => {
+                    const updVol = Math.round((elem.volume + 0.1) * 10) / 10;
+                    const vol = Math.min(1, updVol);
+                    elem.volume = vol;
+                    volume = vol;
+                });
             });
         },
     });
+
+    setInterval(() => {
+        document.querySelectorAll('video, audio').forEach((elem) => (elem.volume = volume));
+    }, 100);
 }
