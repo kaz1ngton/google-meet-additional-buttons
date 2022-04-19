e948d013cb2a6c1b7409008c8eb890f052df1641
Dmitrii Forofontov <forofontov.dev@gmail.com>
2022-04-19 11:52:31 +0200

    fix: fatal error

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-try {
-    browser.browserAction.onClicked.addListener(() => {
-        browser.tabs.executeScript({ file: 'script.js', allFrames: true });
-    });
-} catch (e) {};
-
-try {
-    chrome.browserAction.onClicked.addListener(() => {
-        chrome.tabs.executeScript({ file: 'script.js', allFrames: true });
-    });
-} catch (e) {};
+chrome.browserAction.onClicked.addListener(() => {
+    chrome.tabs.executeScript({ file: 'script.js', allFrames: true });
+});
--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-let volume = 1;
-let buttonsMenu;
-
-const findButtonsMenu = () => {
-    document.querySelectorAll('i').forEach((el) => {
-        if (el.textContent !== 'call_end') return;
-
-        buttonsMenu = el.parentElement.parentElement.parentElement.parentElement;
-    });
-};
-
-if (!buttonsMenu) {
-    findButtonsMenu();
-}
-
-if (buttonsMenu && !document.querySelector('.add-button')) {
-    buttonsMenu.style.display = 'flex';
-    buttonsMenu.style.justifyContent = 'space-between';
+(() => {
+    let volume = 1;
+    let buttonsMenu;
 
     const noop = () => {};
 
-    // function which creates and add no menu button
     const createButton = ({ styleList = {}, content, classes = '', func = noop }) => {
         const button = document.createElement('button');
 
@@ -37,59 +21,70 @@ if (buttonsMenu && !document.querySelector('.add-button')) {
         buttons[1].insertAdjacentElement('afterend', button);
     };
 
-    // creating MUTE / UNMUTE buttons
-    createButton({
-        classes: 'mute-button',
-        content: 'MUTE',
-        func: (button) => {
-            button.addEventListener('click', (e) => {
-                const currentButton = e.currentTarget;
+    document.querySelectorAll('i').forEach((el) => {
+        if (el.textContent !== 'call_end') return;
 
-                if (currentButton.classList.contains('muted')) {
-                    button.innerText = 'MUTE';
-                    document.querySelectorAll('video, audio').forEach((elem) => (elem.muted = false));
-                } else {
-                    button.innerText = 'UNMUTE';
-                    document.querySelectorAll('video, audio').forEach((elem) => (elem.muted = true));
-                }
-                currentButton.classList.toggle('muted');
-            });
-        },
+        buttonsMenu = el.parentElement.parentElement.parentElement.parentElement;
     });
 
-    // creating Volume DOWN button
-    createButton({
-        content: '-',
-        classes: 'volume-button',
-        func: (button) => {
-            button.addEventListener('click', () => {
-                document.querySelectorAll('video, audio').forEach((elem) => {
-                    const updVol = Math.round((elem.volume - 0.1) * 10) / 10;
-                    const vol = Math.max(0, updVol);
-                    elem.volume = vol;
-                    volume = vol;
+    if (buttonsMenu && !document.querySelector('.add-button')) {
+        buttonsMenu.style.display = 'flex';
+        buttonsMenu.style.justifyContent = 'space-between';
+
+        // creating MUTE / UNMUTE buttons
+        createButton({
+            classes: 'mute-button',
+            content: 'MUTE',
+            func: (button) => {
+                button.addEventListener('click', (e) => {
+                    const currentButton = e.currentTarget;
+
+                    if (currentButton.classList.contains('muted')) {
+                        button.innerText = 'MUTE';
+                        document.querySelectorAll('video, audio').forEach((elem) => (elem.muted = false));
+                    } else {
+                        button.innerText = 'UNMUTE';
+                        document.querySelectorAll('video, audio').forEach((elem) => (elem.muted = true));
+                    }
+                    currentButton.classList.toggle('muted');
                 });
-            });
-        },
-    });
+            },
+        });
 
-    // creating Volume UP button
-    createButton({
-        content: '+',
-        classes: 'volume-button',
-        func: (button) => {
-            button.addEventListener('click', () => {
-                document.querySelectorAll('video, audio').forEach((elem) => {
-                    const updVol = Math.round((elem.volume + 0.1) * 10) / 10;
-                    const vol = Math.min(1, updVol);
-                    elem.volume = vol;
-                    volume = vol;
+        // creating Volume DOWN button
+        createButton({
+            content: '-',
+            classes: 'volume-button',
+            func: (button) => {
+                button.addEventListener('click', () => {
+                    document.querySelectorAll('video, audio').forEach((elem) => {
+                        const updVol = Math.round((elem.volume - 0.1) * 10) / 10;
+                        const vol = Math.max(0, updVol);
+                        elem.volume = vol;
+                        volume = vol;
+                    });
                 });
-            });
-        },
-    });
+            },
+        });
 
-    setInterval(() => {
-        document.querySelectorAll('video, audio').forEach((elem) => (elem.volume = volume));
-    }, 100);
-}
+        // creating Volume UP button
+        createButton({
+            content: '+',
+            classes: 'volume-button',
+            func: (button) => {
+                button.addEventListener('click', () => {
+                    document.querySelectorAll('video, audio').forEach((elem) => {
+                        const updVol = Math.round((elem.volume + 0.1) * 10) / 10;
+                        const vol = Math.min(1, updVol);
+                        elem.volume = vol;
+                        volume = vol;
+                    });
+                });
+            },
+        });
+
+        setInterval(() => {
+            document.querySelectorAll('video, audio').forEach((elem) => (elem.volume = volume));
+        }, 100);
+    }
+})();
